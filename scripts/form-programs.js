e0c02d54c843286eb43f8cca8c5b2d63fde76ab5
GabIwa <133536166+GabIwa@users.noreply.github.com>
2023-05-16 23:07:57 -0300

    Update form-programs.js

--- scripts/form-programs.js
+++ scripts/form-programs.js
@@ -9,34 +9,3 @@ function mostrarOcultarCampoTexto() {
       campoTexto.style.display = "none";
     }
 }
-
-//validacion de formulario
-const formulario = document.getElementById('contact-form');
-
-formulario.addEventListener('submit', function(event) {
-  event.preventDefault(); // Evita el envío del formulario
-
-  // Validación de campos
-const nombre = document.getElementById('name');
-const email = document.getElementById('email');
-let valido = true;
-
-if (nombre.value === '') {
-    document.getElementById('error-nombre').textContent = 'El nombre es obligatorio';
-    valido = false;
-  } else {
-    document.getElementById('error-nombre').textContent = '';
-  }
-
-  if (email.value === '') {
-    document.getElementById('error-email').textContent = 'El email es obligatorio';
-    valido = false;
-  } else {
-    document.getElementById('error-email').textContent = '';
-  }
-
-  if (valido) {
-    // Si todos los campos son válidos, puedes realizar alguna acción, como enviar el formulario o realizar otras tareas.
-    formulario.submit();
-  }
-});
